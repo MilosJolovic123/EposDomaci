7b68098103ae11570fa853d1128a8c7fc878e1b2
MilosJolovic123 <jolovicmilos8@gmail.com>
2022-12-21 20:24:03 +0100

    Gluposti

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+var films=document.querySelectorAll(".film");
+
+/*function mOverImg(img){
+    console.log(img.chilNodes);
+    img.style.opacity="0.3";
+    img.chilNodes[3].style.visibility = "visible";
+    //links.style.visibility="visible";
+}
+
+function mOutImg(img,links){
+img.style.opacity="1";
+//links.style.visibility="hidden";
+}*/
+
+films.forEach(function(img,links){
+        console.log(img.childNodes);
+        // img.addEventListener("mouseover", mOverImg(img) );
+        // img.addEventListener("mouseout", mOutImg(img) );
+        img.addEventListener("mouseover", () =>{
+            
+            img.childNodes[1].style.opacity="0.5";
+            img.childNodes[3].style.visibility = "visible";
+            
+        } );
+        img.addEventListener("mouseout", () =>{
+            img.childNodes[1].style.opacity="1";
+            img.childNodes[3].style.visibility = "hidden";
+        } );
+});
+
+
